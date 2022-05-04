7eccada443b8d57916400f09e281dfc8afd5f2f0
dustnzlzl <dustn4325@naver.com>
2022-05-04 11:34:47 +0900

    0504study

---
+++ h18/19237어른상어/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    System.setIn(new FileInputStream("19237어른상어/input.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  }
+}
---
+++ h18/20056마법사상어와파이어볼/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    System.setIn(new FileInputStream("20056마법사상어와파이어볼/input.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  }
+}
