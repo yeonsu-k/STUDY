af03661e4089811f5b5ec5e76bd457dd46194055
dustnzlzl <dustn4325@naver.com>
2022-06-23 03:20:16 +0900

    0621 21회차 - dfs와bfs

--- h21/A17836공주님을구해라/Main.java
+++ h21/A17836공주님을구해라/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
   static class Pos {
     int x, y;
-
     Pos(int x, int y) {
       this.x = x;
       this.y = y;
@@ -33,49 +32,51 @@ public class Main {
 
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine(), " ");
-      for (int j = 0; j < M; j++) {
-        map[i][j] = Integer.parseInt(st.nextToken());
-      }
+      for (int j = 0; j < M; j++) map[i][j] = Integer.parseInt(st.nextToken());
     }
 
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine(), " ");
-      for (int j = 0; j < M; j++) {
-        if (map[i][j] == 0 && !visited[i][j]) {
-          minTime = Math.min(minTime, bfs(i, j));
-        }
-      }
-    }
-    
-    System.out.println(minTime);
+    bfs();
+
+    System.out.println(minTime > T ? "Fail" : minTime);
   }
 
-  private static int bfs(int r,int c) {
+  private static void bfs() {
     Queue<Pos> q = new LinkedList<>();
-    int time = 0;
+    int time = -1;
+    int size = 0;
     
-    q.add(new Pos(r, c));
-    visited[r][c] = true;
-    time++;
+    q.add(new Pos(0,0));
+    visited[0][0] = true;
 
     // 0 : 빈공간 , 1: 벽, 2: 그람(벽을 부술 수 있는 검)
-    while (q.isEmpty()) {
-      //if (time == minTime && time == T) break;
-      Pos pos = q.poll();
-      for (int i = 0; i < 4; i++) {
-        int nx = pos.x + dx[i];
-        int ny = pos.y + dy[i];
-        if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
-          if (map[nx][ny] == 0 && !visited[nx][ny]) {
-            q.add(new Pos(nx, ny));
-            visited[nx][ny] = true;
-            time++;
-            System.out.println();
+    while (!q.isEmpty()) {
+      time++;
+      size = q.size();
+
+      for (int i = 0; i < size; i++) {
+        Pos pos = q.poll();
+
+        if (map[pos.x][pos.y] == 2) { // 검이 있는 자리라면
+          minTime = time + N - 1 - pos.x + M - 1 - pos.y; // 지금까지의 시간 + 두점사이의 거리 구하기
+          continue;
+        } 
+        else if (pos.x == N - 1 && pos.y == M - 1) { // 최종까지 도착했다면
+          minTime = Math.min(minTime, time);
+          return;
+        }
+
+        for (int j = 0; j < 4; j++) {
+          int nx = pos.x + dx[j];
+          int ny = pos.y + dy[j];
+          if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+            if (map[nx][ny] != 1 && !visited[nx][ny]) {
+              q.add(new Pos(nx, ny));
+              visited[nx][ny] = true;
+            }
           }
         }
+        
       }
     }
-    
-    return time;
   }
 }
