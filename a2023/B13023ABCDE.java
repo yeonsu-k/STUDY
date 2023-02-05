771aca57d658deffc8df8bf4d841286a6c099170
dustnzlzl <dustnzlzl@naver.com>
2023-02-06 06:31:16 +0900

    B13023ABCDE

--- a2023/B13023ABCDE.java
+++ a2023/B13023ABCDE.java
@@ -8,7 +8,7 @@ public class B13023ABCDE {
   static boolean result = false;
 
   public static void main(String[] args) throws Exception {
-    System.setIn(new FileInputStream("input.txt"));
+    // System.setIn(new FileInputStream("input.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine());
