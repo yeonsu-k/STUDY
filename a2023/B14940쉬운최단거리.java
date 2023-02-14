5bdb2d87dbbde8b6904e203e99b00282b693f806
dustnzlzl <dustnzlzl@naver.com>
2023-02-14 23:41:51 +0900

    B14940쉬운최단거리

--- a2023/B14940쉬운최단거리.java
+++ a2023/B14940쉬운최단거리.java
@@ -17,7 +17,7 @@ public class B14940쉬운최단거리 {
   }
 
   public static void main(String[] args) throws Exception {
-    System.setIn(new FileInputStream("input.txt"));
+    // System.setIn(new FileInputStream("input.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder sb = new StringBuilder();
 
