ae0101a6053116f01ff0d0c55d2837512a859539
dustnzlzl <dustnzlzl@naver.com>
2023-05-10 18:04:58 +0900

    B2475검증수

---
+++ a2023/구현/B2475검증수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class B2475검증수 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+      sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+    }
+    System.out.println(sum % 10);
+  }
+}
