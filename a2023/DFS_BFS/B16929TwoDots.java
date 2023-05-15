0f995cf637799faddd78ca5a773e05100d793a83
dustnzlzl <dustnzlzl@naver.com>
2023-05-15 12:33:47 +0900

    B16929TwoDots

--- a2023/DFS_BFS/B16929TwoDots.java
+++ a2023/DFS_BFS/B16929TwoDots.java
@@ -15,7 +15,7 @@ public class B16929TwoDots {
   static String result = "No";
 
   public static void main(String[] args) throws IOException {
-    System.setIn(new FileInputStream("input.txt"));/
+    // System.setIn(new FileInputStream("input.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
