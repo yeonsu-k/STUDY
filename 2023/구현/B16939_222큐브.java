5fc5a63d381f71832cfba7c4adb9f7c885970a1d
dustnzlzl <dustnzlzl@naver.com>
2023-06-01 02:19:54 +0900

    Update B16939_222큐브 : 코드길이 단축

--- 2023/구현/B16939_222큐브.java
+++ 2023/구현/B16939_222큐브.java
@@ -3,6 +3,14 @@ import java.util.*;
 
 public class B16939_222큐브 {
   static int[] cube = new int[25];
+  static int[][][][] rubiks = {
+      { { { 13, 14 }, { 5, 6 }, { 17, 18 }, { 21, 22 } },
+          { { 15, 16 }, { 7, 8 }, { 19, 20 }, { 23, 24 } } },
+      { { { 1, 3 }, { 5, 7 }, { 9, 11 }, { 22, 24 } },
+          { { 2, 4 }, { 6, 8 }, { 10, 12 }, { 21, 23 } } },
+      { { { 1, 2 }, { 18, 20 }, { 11, 12 }, { 13, 15 } },
+          { { 3, 4 }, { 17, 19 }, { 9, 10 }, { 14, 16 } } }
+  };
 
   public static void main(String[] args) throws Exception {
     // System.setIn(new FileInputStream("input.txt"));
@@ -16,31 +24,12 @@ public class B16939_222큐브 {
     // 각 1~4, 9~12 같은 색일 경우 가로 회전
     // 각 13~16, 17~20 같은 색일 경우 세로 회전
     // 각 5~8, 21,24 같은 색일 경우 옆사이드로 회전
-    String type = setType();
-    if (type.equals("length")) {
-      // 가로 회전 가능할 경우
-      int[][][] rubiks = { { { 13, 14 }, { 5, 6 }, { 17, 18 }, { 21, 22 } },
-          { { 15, 16 }, { 7, 8 }, { 19, 20 }, { 23, 24 } } };
-      if (!(testSetting(rubiks[0], rubiks[1]) ^ testSetting(rubiks[1], rubiks[0]))) {
-        type = "none";
-      }
-    } else if (type.equals("width")) {
-      // 세로 회전 가능할 경우
-      int rubiks[][][] = { { { 1, 3 }, { 5, 7 }, { 9, 11 }, { 22, 24 } },
-          { { 2, 4 }, { 6, 8 }, { 10, 12 }, { 21, 23 } } };
-      if (!(testSetting(rubiks[0], rubiks[1]) ^ testSetting(rubiks[1], rubiks[0]))) {
-        type = "none";
-      }
-    } else if (type.equals("side")) {
-      // 옆사이드로 회전 가능할 경우
-      int rubiks[][][] = { { { 1, 2 }, { 18, 20 }, { 11, 12 }, { 13, 15 } },
-          { { 3, 4 }, { 17, 19 }, { 9, 10 }, { 14, 16 } } };
-      if (!(testSetting(rubiks[0], rubiks[1]) ^ testSetting(rubiks[1], rubiks[0]))) {
-        type = "none";
-      }
+    int type = setType();
+    if (type != 3 && !(testSetting(rubiks[type][0], rubiks[type][1]) ^ testSetting(rubiks[type][1], rubiks[type][0]))) {
+      type = 3;
     }
 
-    System.out.println(type.equals("none") ? 0 : 1);
+    System.out.println(type == 3 ? 0 : 1);
   }
 
   private static boolean testSetting(int[][] x, int[][] y) {
@@ -54,10 +43,10 @@ public class B16939_222큐브 {
     return true;
   }
 
-  private static String setType() {
+  private static int setType() {
     int[][] all = { { 1, 9 }, { 13, 17 }, { 5, 21 } };
     int color = 0;
-    String type = "none";
+    int type = 3;
     boolean[] setting = { true, true, true };
 
     for (int i = 0; i < 3; i++) {
@@ -74,7 +63,7 @@ public class B16939_222큐브 {
 
     // 셋 중 하나만 true 일 때
     if (setting[0] ^ setting[1] ^ setting[2]) {
-      type = setting[0] ? "length" : setting[1] ? "width" : "side";
+      type = setting[0] ? 0 : setting[1] ? 1 : 2;
     }
 
     return type;
