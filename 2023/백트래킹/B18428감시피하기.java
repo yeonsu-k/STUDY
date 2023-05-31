357a677654ab91795dd1ca987f778a1e36179264
dustnzlzl <dustnzlzl@naver.com>
2023-06-01 01:00:22 +0900

    B18428감시피하기

--- 2023/백트래킹/B18428감시피하기.java
+++ 2023/백트래킹/B18428감시피하기.java
@@ -1,102 +1,81 @@
 import java.io.*;
 import java.util.*;
 
-class Pos {
-  int x, y;
-
-  Pos(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
 class B18428감시피하기 {
-  static int[] dx = { -1, 0, 1, 0 }, dy = { 0, 1, 0, -1 };
+  static class Pos {
+    int x, y;
+
+    Pos(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
   static int N;
-  static char[][] mapX, mapY, copyMapX, copyMapY; // 가로형 세로형 맵
-  static ArrayList<Pos> gap;
-  static String answer = "NO";
+  static char[][] map;
+  static ArrayList<Pos> per;
 
   public static void main(String[] args) throws IOException {
-    System.setIn(new FileInputStream("input.txt"));
+    // System.setIn(new FileInputStream("input.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     N = Integer.parseInt(br.readLine());
-    mapX = new char[N][N];
-    mapY = new char[N][N];
-    copyMapX = new char[N][N];
-    copyMapY = new char[N][N];
-    gap = new ArrayList<>();
+    map = new char[N][N];
+    per = new ArrayList<>();
 
     for (int i = 0; i < N; i++) {
       String str = br.readLine().replace(" ", "");
       if (check(str)) {
-        System.out.print(answer);
+        System.out.print("NO");
         return;
       }
       for (int j = 0; j < N; j++) {
         char ch = str.charAt(j);
-        mapX[i][j] = ch;
-        mapY[j][i] = ch;
+        map[i][j] = ch;
         if (ch == 'X')
-          gap.add(new Pos(i, j));
+          per.add(new Pos(i, j));
       }
     }
 
-    for (int i = 0; i < N; i++) {
-      copyMapX[i] = mapX[i].clone();
-      copyMapY[i] = mapY[i].clone();
-    }
+    nCr(0, 0);
 
-    nCr(0, 0, copyMapX, copyMapY);
-
-    System.out.print(answer);
+    System.out.print("NO");
   }
 
-  private static void nCr(int start, int cnt, char[][] copyMapX, char[][] copyMapY) {
-    if (answer.equals("YES"))
-      return;
+  private static void nCr(int start, int cnt) {
     if (cnt == 3) {
+      char[][] copyMap = new char[N][N];
+      for (int j = 0; j < N; j++) {
+        for (int k = 0; k < N; k++) {
+          copyMap[j][k] = map[k][j];
+        }
+      }
+
       boolean result = true;
       for (int i = 0; i < N; i++) {
-        String strX = String.valueOf(copyMapX[i]).replace("X", "");
-        String strY = String.valueOf(copyMapY[i]).replace("X", "");
-        result &= !check(strX) & !check(strY); // 감시 성공하면
+        String strX = String.valueOf(map[i]).replace("X", "");
+        String strY = String.valueOf(copyMap[i]).replace("X", "");
+        result &= !check(strX) & !check(strY);
       }
+
       if (result) {
-        // 모든 감시가 성공하면
-        answer = "YES";
-        for (int j = 0; j < N; j++) {
-          System.out.println(Arrays.toString(copyMapX[j]) + "     " + Arrays.toString(copyMapY[j]));
-        }
-        System.out.println();
-        return;
+        System.out.println("YES");
+        System.exit(0);
       }
 
       return;
     }
 
-    for (int i = start; i < gap.size(); i++) {
-      Pos pos = gap.get(i);
-      copyMapX[pos.x][pos.y] = 'O';
-      copyMapY[pos.y][pos.x] = 'O';
-
-      String strX = String.valueOf(copyMapX[pos.x]).replace("X", "");
-      String strY = String.valueOf(copyMapY[pos.y]).replace("X", "");
-      if (!check(strX) ^ !check(strY)) {
-
-        copyMapX[pos.x][pos.y] = 'X';
-        copyMapY[pos.y][pos.x] = 'X';
-        continue;
-      }
-
-      nCr(i + 1, cnt + 1, copyMapX, copyMapY);
-      copyMapX[pos.x][pos.y] = 'X';
-      copyMapY[pos.y][pos.x] = 'X';
+    for (int i = start; i < per.size(); i++) {
+      Pos pos = per.get(i);
+      map[pos.x][pos.y] = 'O';
+      nCr(i + 1, cnt + 1);
+      map[pos.x][pos.y] = 'X';
     }
   }
 
   private static boolean check(String str) {
     return str.contains("TS") || str.contains("ST");
   }
+
 }
