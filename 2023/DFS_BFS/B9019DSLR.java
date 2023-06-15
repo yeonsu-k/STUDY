354b9520b8ffb74cd7bc4c4b9f05e036e44793ba
dustnzlzl <dustnzlzl@naver.com>
2023-06-16 01:48:49 +0900

    B9019DSLR

--- 2023/DFS_BFS/B9019DSLR.java
+++ 2023/DFS_BFS/B9019DSLR.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class B9019DSLR {
-  static String result;
 
   static class DSLR {
     String type;
@@ -15,7 +14,7 @@ public class B9019DSLR {
   }
 
   public static void main(String[] args) throws IOException {
-    System.setIn(new FileInputStream("input.txt"));
+    // System.setIn(new FileInputStream("input.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder sb = new StringBuilder();
 
@@ -24,45 +23,50 @@ public class B9019DSLR {
       StringTokenizer st = new StringTokenizer(br.readLine());
       int A = Integer.parseInt(st.nextToken());
       int B = Integer.parseInt(st.nextToken());
-      result = "";
-      BFS(A, B);
-      sb.append(result).append('\n');
+      sb.append(BFS(A, B)).append('\n');
     }
 
     System.out.print(sb);
   }
 
-  private static void BFS(int a, int b) {
-    Queue<DSLR> que = new LinkedList<>();
-    boolean[] visited = new boolean[10000]; // BFS 탐색의 방문 여부 체크
-    que.add(new DSLR("", a));
-    visited[a] = true;
+  private static String BFS(int a, int b) {
+    Queue<Integer> que = new LinkedList<>();
+    char[] command = new char[10000]; // 명령어 저장
+    int[] from = new int[10000]; // 경로 저장
+    que.offer(a);
 
     while (!que.isEmpty()) {
-      DSLR now = que.poll();
-      for (DSLR nx : calcN(now.num)) {
-        if (visited[nx.num])
-          continue;
-        if (b == nx.num) {
-          result = now.type + nx.type;
-          return;
+      int now = que.poll();
+      for (int i = 0; i < 4; i++) {
+        int nx = calcN(i, now);
+        if (command[nx] == 0) {
+          command[nx] = i == 0 ? 'D' : i == 1 ? 'S' : i == 2 ? 'L' : 'R';
+          from[nx] = now;
+          que.add(nx);
         }
-        visited[nx.num] = true;
-        que.add(new DSLR(now.type + nx.type, nx.num));
+        if (nx == b) {
+          StringBuilder sb = new StringBuilder();
+          int temp = b;
+          while (temp != a) {
+            sb.append(command[temp]);
+            temp = from[temp];
+          }
+          return sb.reverse().toString();
+        }
       }
     }
+
+    return null;
+
   }
 
-  private static DSLR[] calcN(int now) {
-    int D = (2 * now) % 10000;
-    int S = (now == 0) ? 9999 : now - 1;
-    int L = (now % 1000) * 10 + now / 1000;
-    int R = (now % 10) * 1000 + now / 10;
-    return new DSLR[] {
-        new DSLR("D", D),
-        new DSLR("S", S),
-        new DSLR("L", L),
-        new DSLR("R", R)
-    };
+  private static int calcN(int i, int now) {
+    if (i == 0)
+      return (2 * now) % 10000;
+    if (i == 1)
+      return (now == 0) ? 9999 : now - 1;
+    if (i == 2)
+      return (now % 1000) * 10 + now / 1000;
+    return (now % 10) * 1000 + now / 10;
   }
 }
